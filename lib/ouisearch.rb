08d256f0cbd37075ff2a795a6d7fb6780fecd732
enukane <enukane@glenda9.org>
2015-01-07 14:11:27 +0900

    brush up
    Signed-off-by: enukane <enukane@glenda9.org>

--- lib/ouisearch.rb
+++ lib/ouisearch.rb
@@ -1,5 +1,75 @@
 require "ouisearch/version"
+require "faraday"
 
-module Ouisearch
-  # Your code goes here...
+class OuiSearch
+  URL="www.ieee.org/netstorage/standards/oui.txt"
+  URLHOST="http://www.ieee.org"
+  URLPATH="/netstorage/standards/oui.txt"
+  CACHE="#{ENV['HOME']}/.oui"
+  REG=/^\s+(..)-(..)-(..)\s+\(hex\)\s+(.+)$/
+
+  def initialize args
+    @reload = args[:reload] || false
+    @debug = args[:debug] || false
+    @ouis = nil
+    fetch_cache
+    load_cache
+  end
+
+  def execute oui
+    vendor = @ouis[oui.upcase]
+    if vendor
+      return vendor
+    else
+      return "<UNKNOWN>"
+    end
+  end
+
+  def fetch_cache
+    if File.exists?(CACHE) and @reload == false
+      # nothing to do
+      return
+    end
+
+    if @reload
+      dprint "force cache update ($HOME/.oui) ...\n"
+    else
+      dprint "no cache ($HOME/.oui) found. creating cache ...\n"
+    end
+
+    File.open(CACHE, "w") do |f|
+      @ouis = http_get_ouis()
+      f.write(Marshal.dump(@ouis))
+    end
+
+    dprint "cache created (#{@ouis.length} entries)\n"
+  end
+
+  def http_get_ouis
+    ouis = {}
+    conn = Faraday.new(:url => URLHOST)
+    response = conn.get(URLPATH)
+    raise "failed to fetch oui" if response.status != 200
+    response.body.split("\n").each do |line|
+      match = line.match(REG)
+      next unless match
+      oui = match[1] + ":" + match[2] + ":" + match[3]
+      vendor = match[4].strip
+      ouis[oui.upcase] = vendor
+    end
+    return ouis
+  end
+
+  def load_cache
+    return @ouis if @ouis # already fetched
+    File.open(CACHE, "r") do |f|
+      @ouis = Marshal.load(f.read)
+    end
+    dprint "cache reloading done (#{@ouis.length} entries)\n"
+  end
+
+  def dprint str
+    return nil unless @debug
+    print "#{str}\n"
+  end
 end
--- ouisearch.gemspec
+++ ouisearch.gemspec
@@ -21,5 +21,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.5"
   spec.add_development_dependency "rake"
 
-  spec.add_dependency 'httpclient'
+  spec.add_dependency 'faraday'
 end
